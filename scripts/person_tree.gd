0eb322c67f8396800d99666d85e03aecc8f54cd0
SaintAnd <utunittinge@mail.ru>
2023-07-20 21:26:35 +0300

    Добавил скрипт и персонаж, корень

---
+++ scripts/person_tree.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var speed = 200
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("Up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("Down"):
+		velocity.y += speed
+	if Input.is_action_pressed("Left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("Right"):
+		velocity.x += speed
+	move_and_slide(velocity)
+	position.x = clamp(position.x, 0,500)
+	position.y = clamp(position.y, 0,500)
+	
