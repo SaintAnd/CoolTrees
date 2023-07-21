b39fb875b57f6e6f7df03ca67300a739f3c111bc
MorkiGrok <140083846+MorkiGrok@users.noreply.github.com>
2023-07-21 19:57:22 +0300

    Add files via upload

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,28 +1,48 @@
+class_name ZoomingCamera2D
 extends Camera2D
 
-onready var zoomfactor = 1 # На сколько далеко отдаляется камера
-onready var zoomspeed = 20 # Скорость отдаления
-onready var zoomstep = 0.03 # Шаг приближения
-onready var factorstep = 0.01 # Скорость прекращения приближения
 
-func _ready():
-	pass
+# Lower cap for the `_zoom_level`.
+export var min_zoom := 0.5
+# Upper cap for the `_zoom_level`.
+export var max_zoom := 2.0
+# Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
+export var zoom_factor := 0.1
+# Duration of the zoom's tween animation.
+export var zoom_duration := 0.2
 
-func _process(delta):
-	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomspeed * delta)
-	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomspeed * delta)
-	
-	zoom.x = clamp(zoom.x, 0.5,2)
-	zoom.y = clamp(zoom.y, 0.5,2)
-	
-	if zoomfactor > 1:
-		zoomfactor -= zoomstep
-	elif zoomfactor < 1:
-		zoomfactor += zoomstep
+# The camera's target zoom level.
+var _zoom_level := 1.0 setget _set_zoom_level
+
+# We store a reference to the scene's tween node.
+onready var tween: Tween = $Tween
+
+func _set_zoom_level(value: float) -> void:
+	# We limit the value between `min_zoom` and `max_zoom`
+	_zoom_level = clamp(value, min_zoom, max_zoom)
+	# Then, we ask the tween node to animate the camera's `zoom` property from its current value
+	# to the target zoom level.
+	tween.interpolate_property(
+		self,
+		"zoom",
+		zoom,
+		Vector2(_zoom_level, _zoom_level),
+		zoom_duration,
+		tween.TRANS_SINE,
+		# Easing out means we start fast and slow down as we reach the target value.
+		tween.EASE_OUT
+	)
+	tween.start()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("zoom_in"):
+		# Inside a given class, we need to either write `self._zoom_level = ...` or explicitly
+		# call the setter function to use it.
+		_set_zoom_level(_zoom_level - zoom_factor)
+	if event.is_action_pressed("zoom_out"):
+		_set_zoom_level(_zoom_level + zoom_factor)
 		
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_WHEEL_UP:
-			zoomfactor -= zoomstep
-		elif event.button_index == BUTTON_WHEEL_DOWN:
-			zoomfactor = zoomstep
+
+	
+
+	
