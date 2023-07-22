b1ca55a2adffe2e1c9582802810be859a9cc9818
Gorkimork <alextepik@mail.ru>
2023-07-22 19:01:43 +0300

    Некоторые изменения

--- scripts/Nature.gd
+++ scripts/Nature.gd
@@ -22,13 +22,13 @@ func _process(delta):
 	pass
 	
 func move_editor():
-	if Input.is_action_pressed(""):
+	if Input.is_action_pressed("w"):
 		editor.global_position.y -= cam_spd
-	if Input.is_action_pressed(""):
+	if Input.is_action_pressed("a"):
 		editor.global_position.x -= cam_spd
-	if Input.is_action_pressed(""):
+	if Input.is_action_pressed("s"):
 		editor.global_position.y += cam_spd
-	if Input.is_action_pressed(""):
+	if Input.is_action_pressed("d"):
 		editor.global_position.x += cam_spd
 	pass
 	
