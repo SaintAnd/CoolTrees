f52ac05f864a78329a2430ffe8e606c7f45a9ba4
maksoblako31 <maksoblako31@gmail.com>
2023-07-27 04:17:41 +0300

    я доделал настройки звука: мастер весь звук, мьюзик в самой игре, а саундФХ в меню(сразу легко можно заметить, если подёргать)

--- scripts/Main_menu.gd
+++ scripts/Main_menu.gd
@@ -3,6 +3,8 @@ onready var menu = $Menu
 onready var settings = $Settings
 onready var video = $Video
 onready var audio = $Audio
+const MUSIC_BUS = "Music"
+const FX_BUS = "SoundFX"
 func _ready():
 	pass
 
@@ -59,20 +61,27 @@ func _on_VSync_toggled(button_pressed):
 func _on_BackFromVideo_pressed():
 	show_and_hide(settings, video)
 
-
 func _on_Master_value_changed(value):
-	volume(0, value)
+	if value == -45:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+	AudioServer.set_bus_volume_db(0,value)
+
 	
 func volume(bus_index, value):
 	AudioServer.set_bus_volume_db(bus_index, value)
 
 
 func _on_Music_value_changed(value):
-	volume(1, value)
+	var BusInt = AudioServer.get_bus_index(MUSIC_BUS)
+	AudioServer.set_bus_volume_db(BusInt, value)
 
 func _on_SoundFX_value_changed(value):
-	volume(2, value)
-
+	var BusInt = AudioServer.get_bus_index(FX_BUS)
+	AudioServer.set_bus_volume_db(BusInt, value)
 
 func _on_BackFromAudio_pressed():
 	show_and_hide(settings, audio)
+
+
