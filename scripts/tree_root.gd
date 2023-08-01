f7432eb1328179044d21e797395bf620718880f2
Antreri <artem.shapoval.00@inbox.ru>
2023-08-01 13:39:48 +0300

    И снова система корней с зачатками системы сбора ресурсов

--- scripts/tree_root.gd
+++ scripts/tree_root.gd
@@ -1,34 +1,49 @@
 extends Node2D
 
+
 var a = Vector2()#начальная точка корня
 var b = Vector2()#конечная точка корня
-var c = Vector2()#точка отклонения(к ней корень отклоняется на пути между начальной и конечной точкой)
-var s = Vector2(-1,1)#этот вектор нужен для создания точки отклонения
-var con#длина вектора соеденяющего вектора a и b
-var t = 0.15#скорость интерполяции из точки a в точку b
-var flag = -1#этот флаг отвечает за то чтобы точка отклонения была то слева то справа
-
-func interpolate(p0:Vector2,p1:Vector2,p2:Vector2,t):
-	var q0 = p0.linear_interpolate(p1,t)# функция спизжена отсюда https://docs.godotengine.org/ru/stable/tutorials/math/beziers_and_curves.html
-	var q1 = p1.linear_interpolate(p2,t)
-	var r = q0.linear_interpolate(q1,t)
-	return r
+var c = Vector2()
+var t = 0.0
+var f = 0.0
+var count = 0
+var flag1 = true
+var flag2 = false
+onready var curve = $path.curve
+onready var point = $path/pathf
+onready var tip = $path/pathf/tip
+onready var roots = $roots.curve
+onready var look = $look_of_roots
 
 func _process(delta):
-	a = $tip.position
-	if Input.is_action_just_pressed("mouse_left_click"):#конечная точка задается когда игрок нажал левой кнопкой мыши
-		b = get_local_mouse_position()#конечной точкой является место куда кликнул игрок
-		flag *= -1#попеременно меняем флаг на левый и правый
+	if Input.is_action_just_pressed("button_left") and curve.get_point_count() < 2:
+		if flag1:
+			a = point.position
+			curve.add_point(a)
+			roots.add_point(a)
+			look.add_point(a)
+			flag1 = false
+		b = get_local_mouse_position()
+		flag2 = true
+		t = 0
+		count +=1
+		curve.add_point(b)
+		roots.add_point(b)
+		look.add_point(a)
+	if b:
+		point.position = curve.interpolate(0,t)
 		
-	if b:#если вектор b инициализирован
-		c = b - a#задаем вектору с вектор соеденяющий a и b
-		con = (sqrt(c.x * c.x + c.y * c.y))/2#узнаем длину вектора c, вот и Теорема Пифагора пригодилась:>
-		c.x /=2#сокращаем вектор в два раза и находим середину
-		c.y /=2
-		c = c + (s * con/2 * flag) #смещаем относительно вектора соеденяющего a и b
-		c = c + a
-		
-		$tip.position = interpolate(a,c,b,t)
-		
-
-
+		look.set_point_position(count,point.position)
+	
+	if !is_equal_approx(t,1) and b:
+		t+=0.01
+	elif curve.get_point_count() == 2:
+		curve.remove_point(0)
+		flag2 = false
+	
+	#$roots/pulsar.unit_offset = 1 - t 
+	#if flag2:
+	#	f+=0.1
+	#	tip.look_at(b)
+	#	c.y = sin(f) * 2
+	#	tip.translate(c)
