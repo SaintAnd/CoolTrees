720abbd31529e920999312afe2783a6e6b93c78c
TH3D3LTA <140918353+TH3D3LTA@users.noreply.github.com>
2023-08-14 15:42:04 +0300

    Update from main branch
    Обновил ветку + поменял меню, добавил кириллицу

--- scripts/MenuButton.gd
+++ scripts/MenuButton.gd
@@ -1,25 +1,33 @@
 extends MenuButton
 
+
+#Добавляем варианты
 func _ready():
-	get_popup().add_item("Build")
-	get_popup().add_item("Refresh")
-	get_popup().add_item("Settings")
-	get_popup().add_item("Quit")
-	
-	
+	get_popup().add_item("Строительство")
+	get_popup().add_item("Перезапуск")
+	get_popup().add_item("Настройки")
+	get_popup().add_item("Выйти в меню")
+	get_popup().add_item("Выйти на рабочий стол")
 	get_popup().connect('id_pressed', self, '_on_item_pressed')
 
 
-
+#Проверяем какая кнопка нажата
 func _on_item_pressed(id):
 	var item_name = get_popup().get_item_text(id)
-	if item_name == 'Build':
+	
+	if item_name == 'Строительство':
 		get_node('/root/Root/BuildMenu').visible = true
-	elif item_name == 'Refresh':
+		
+	elif item_name == 'Перезапуск':
 		get_tree().change_scene('res://scenes/Root.tscn')
-	elif item_name == 'Settings':
+		
+	elif item_name == 'Настройки':
 		get_node('/root/Root/Settings').visible = true
-	elif item_name == 'Quit':
+		
+	elif item_name == 'Выйти в меню':
+		get_tree().change_scene('res://scenes/Main_menu.tscn')
+		
+	elif item_name == 'Выйти на рабочий стол':
 		get_tree().quit()
 
 
