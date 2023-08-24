e7352f6923f7b28ab72173f4b088251613da2b2f
Dotik125 <sonnadrimur125@gmail.com>
2023-08-24 17:12:40 +1000

    в своей ветке понатылкала комментарии

--- scripts/Worker.gd
+++ scripts/Worker.gd
@@ -1,27 +1,24 @@
 extends KinematicBody2D
+# скрипт для передвижения рабочего и всяких штучек (свечение, выбор и отмена выбора,смена персика)
 
-var speed = 200 # скорость игрока
-var gravity = 150 # сила гравитации
-var state = "worker_idle" 
-var selected = false
 
-onready var anim = $Sprite/Anim # определение аниматора
-onready var glow = $Sprite/Glow 
-onready var tween = $Sprite/Glow/Tween
-onready var manager = get_parent().get_parent()
+var speed = 200 # скорость рабочего
+var gravity = 150 # сила гравитации (но пока что рабочий никуда не падает, так что это пока бесполезно)
+var state = "worker_idle" 	# сохраняем начальную анимацию рабочего
+var selected = false	# переменная будет отвечать за то, выбран ли работник при помощи прямоугольного выделения
+var room = StaticBody2D 	# тут будем хранить комнату в которой находится этот рабочий
+
+onready var anim = $Sprite/Anim # аниматор рабочего
+onready var glow = $Sprite/Glow 	# свет при помощи которого будет выделяться активный рабочий
+onready var glow_tween = $Sprite/Glow/Tween 	# аниматор для плавного включения и отключения свечения
+onready var manager = get_parent().get_parent()	# объект Worker_Manager имеющий необходимые для работы переменные и функции
 onready var elevator = 0.0
 onready var elevator_anim = AnimationPlayer
+
 
 func _ready():
 	anim.play("worker_idle") # проигрываем анимацию сразу после начала игры
 
-func select():
-	selected = true
-	modulate = Color.aquamarine
-	
-func deselect():
-	selected = false
-	modulate = Color.white
 
 func room_define(body):
 	if body.is_in_group("Room"):
@@ -29,33 +26,35 @@ func room_define(body):
 		elevator_anim = body.get_child(3).get_child(0)
 
 
-func glowing(alpha):
-	if alpha == 1: 
-		tween.interpolate_property(glow, "color", glow.color, Color(0.95, 0.65, 0.28, 1), 0.4)
-		tween.start()
-	else:
-		tween.interpolate_property(glow, "color", glow.color, Color(0.95, 0.65, 0.28, 0), 0.08)
-		tween.start()
+func glowing(alpha):	# функция свечения вызываемая по окнончанию таймера (по задумке, но пока функция через скрипт работает) 
+	if alpha == 1: 	# если передано значение альфы 1
+		glow_tween.interpolate_property(glow, "color", glow.color, Color(0.95, 0.65, 0.28, 1), 0.4)	# настраиваем
+		glow_tween.start() 	# запускаем анимку
+	else: 	# если передано значение 0
+		glow_tween.interpolate_property(glow, "color", glow.color, Color(0.95, 0.65, 0.28, 0), 0.08)	# тоже настраиваем
+		glow_tween.start() 	# тооже запускаем
+		
 		
 func _physics_process(_delta):
 	var velocity = Vector2() # определяем велосити
 	if manager.active_worker == name and manager.is_active_w: # проверка на активность данного рабочего
-		if Input.is_action_pressed("Left"): 
-			velocity.x -= speed
+		if Input.is_action_pressed("Left"): 	# если нажато действие влево
+			velocity.x -= speed 	# вычитаем переменную скорости из велосити по х, ну то есть из силы толчка (хихи хаха толчок) в моем понимании) 
 		if Input.is_action_pressed("Right"):
-			velocity.x += speed
-			
-		if velocity.x < 0: # меняем состояния
-			state_change("move_left")
-			glowing(0)
+			velocity.x += speed 	# ну а тут аналогично прибавляем чтобы рабочий вправо бежал
+		
+		# меняем состояния	
+		if velocity.x < 0: 	# если значение велосити отрицательное (то есть работник влево двигается
+			state_change("move_left")  	# включаем анимку движения влево
+			glowing(0) 	# отключаем подсветку
 		elif velocity.x > 0:
-			state_change("move_right")
+			state_change("move_right") 	# тут анологично
 			glowing(0)
-		else:
-			state_change("worker_idle")
-			glowing(1)
-	else:
-		glowing(0)
+		else: 	# если рабочий не двигается
+			state_change("worker_idle") 	# включаем анимацию бездействия
+			glowing(1) 	# включаем подсветку
+	else: 	# если данный рабочий не активен
+		glowing(0) 	# вырубаем подсветку
 	
 	velocity.y += gravity # создаем гравитацию
 	
@@ -75,3 +74,11 @@ func state_change(new_state): # проигрываем анимацию
 func _on_ChangePlayer_pressed(): # переключение на персонажа по нажатию
 	manager.active_worker = name # меняем активного рабочего
 	glowing(1)
+
+func select(): 	# функция вызываемая при выборе работника при помощи прямоугольного выделения
+	selected = true 	# меняем значение переменной
+	modulate = Color.aquamarine	# пока что смена цвета рабочего ведь со свечением я пока не разобралась
+	
+func deselect(): 	# отмена выбора рабочего
+	selected = false
+	modulate = Color.white 	# возращаем цвет на нормальный
