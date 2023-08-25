b95f7fca449c4b016c3dc72104321c824e45bfd1
GospodinAnd <tepdron97@mail.ru>
2023-08-25 16:02:52 +0300

    Добавил перемнную для задания количества проходов генерации

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -33,11 +33,10 @@ func _ready():
 	set_cell(4,3,1)
 	set_cell(5,3,1)
 	set_cell(6,3,1)
-	
-func _process(delta):
 	# Задаём клеточному автомату размер и место
-	automataTemplate(200, 50, map_gen_ore_position_x, map_gen_ore_position_y,1)
-	
+	automataTemplate(10,200, 50, map_gen_ore_position_x, map_gen_ore_position_y,1)
+		
+func _process(delta):
 	# Подсчёт кадров для отладки в консоли
 	frame +=1
 	print ("Кадры: " + str(frame))
@@ -112,7 +111,7 @@ func generate_ground(width, start_height, end_height, chance, tile_index):
 				set_cell(x, y, tile_index) # Замещаем ячейку на нужную
 				chance += 0.001
 
-func automataTemplate(width,height,px,py,ON=2,OFF=0):
+func automataTemplate(iteration,width,height,px,py,ON=2,OFF=0):
 # px - Начальная позиция по x
 # py - Начальная позиция по y
 # width - ширина поля
@@ -123,57 +122,59 @@ func automataTemplate(width,height,px,py,ON=2,OFF=0):
 # Добавим к ширине и высоте начальные позиции
 	width = int(width + px)
 	height = int(height + py) 
-# Обнуляем клеточному автомату сетки жизни и смерти
-	var grid_life = []
-	var grid_death = []
-# Вводим константы включеных и отключеных клеток
+	# Выбираем количество проходов цикла
+	for _i in iteration:
+	# Обнуляем клеточному автомату сетки жизни и смерти
+		var grid_life = []
+		var grid_death = []
+	# Вводим константы включеных и отключеных клеток
 
-# Проходим по полю ширины и высоты
-	for x in range (px,width):
-		for y in range (py,height):
-			# Сбрасываем счётчик соседей на нуль
-			var neighbors = 0
-			# Проверяме соседей с 8-ми сторон живы ли они
-			# Если да, то добавляем в счётчик
-			# Делаем фигуру "тор" с помощью деления по модулю
-			if get_cell(x,(y-1)%height) == ON:			neighbors += 1 # /\
-#			print("/\\" + str(neighbors))
-			if get_cell(x,(y+1)%height) == ON:			neighbors += 1 # \/
-#			print("\\/" + str(neighbors))
-			if get_cell((x-1)%width,y) == ON:			neighbors += 1 # <-
-#			print("<-" + str(neighbors))
-			if get_cell((x+1)%width,y) == ON:			neighbors += 1 # ->
-#			print("->" + str(neighbors))
-			if get_cell((x-1)%width,(y-1)%height) == ON:neighbors += 1 # Г
-#			print("Г" + str(neighbors))
-			if get_cell((x-1)%width,(y+1)%height) == ON:neighbors += 1 # L
-#			print("L" + str(neighbors))
-			if get_cell((x+1)%width,(y-1)%height) == ON:neighbors += 1 # /|
-#			print("/|" + str(neighbors))
-			if get_cell((x+1)%width,(y+1)%height) == ON:neighbors += 1 # _|
-#			print("_|" + str(neighbors))
-			
-			# Здесь главное условие игры B3/S23
-			# Если 3 живых клетки рядом, то создаём живую вместо мёртвой
-			# Если рядом меньше 2-ух и больше 3-ёх, то убиваем клетку
-			if get_cell(x, y) == ON:
-				if neighbors < 4 :
-					grid_death.append(Vector2(x,y))
-			else:
-				if neighbors == 3 :
-					grid_life.append(Vector2(x,y))
-#				if (neighbors > 4) or (neighbors < 9) :
-#					grid_life.append(Vector2(x,y))
-# Проходим по таблице смерти и убиваем клетки
-	for t in grid_death:
-		set_cellv(t,OFF)
-#		print ("end_DEL:" + str(t))
-#	print (grid_death)
-# Проходим по таблице жизни и оживляем клетки
-	for t in grid_life:
-		set_cellv(t,ON)
-#		print ("end_ON:" + str(t))
-#	print (str(grid_life))
+	# Проходим по полю ширины и высоты
+		for x in range (px,width):
+			for y in range (py,height):
+				# Сбрасываем счётчик соседей на нуль
+				var neighbors = 0
+				# Проверяме соседей с 8-ми сторон живы ли они
+				# Если да, то добавляем в счётчик
+				# Делаем фигуру "тор" с помощью деления по модулю
+				if get_cell(x,(y-1)%height) == ON:			neighbors += 1 # /\
+	#			print("/\\" + str(neighbors))
+				if get_cell(x,(y+1)%height) == ON:			neighbors += 1 # \/
+	#			print("\\/" + str(neighbors))
+				if get_cell((x-1)%width,y) == ON:			neighbors += 1 # <-
+	#			print("<-" + str(neighbors))
+				if get_cell((x+1)%width,y) == ON:			neighbors += 1 # ->
+	#			print("->" + str(neighbors))
+				if get_cell((x-1)%width,(y-1)%height) == ON:neighbors += 1 # Г
+	#			print("Г" + str(neighbors))
+				if get_cell((x-1)%width,(y+1)%height) == ON:neighbors += 1 # L
+	#			print("L" + str(neighbors))
+				if get_cell((x+1)%width,(y-1)%height) == ON:neighbors += 1 # /|
+	#			print("/|" + str(neighbors))
+				if get_cell((x+1)%width,(y+1)%height) == ON:neighbors += 1 # _|
+	#			print("_|" + str(neighbors))
+				
+				# Здесь главное условие игры B3/S23
+				# Если 3 живых клетки рядом, то создаём живую вместо мёртвой
+				# Если рядом меньше 2-ух и больше 3-ёх, то убиваем клетку
+				if get_cell(x, y) == ON:
+					if neighbors < 4 :
+						grid_death.append(Vector2(x,y))
+				else:
+					if neighbors == 3 :
+						grid_life.append(Vector2(x,y))
+	#				if (neighbors > 4) or (neighbors < 9) :
+	#					grid_life.append(Vector2(x,y))
+	# Проходим по таблице смерти и убиваем клетки
+		for t in grid_death:
+			set_cellv(t,OFF)
+	#		print ("end_DEL:" + str(t))
+	#	print (grid_death)
+	# Проходим по таблице жизни и оживляем клетки
+		for t in grid_life:
+			set_cellv(t,ON)
+	#		print ("end_ON:" + str(t))
+	#	print (str(grid_life))
 
 # Модель генерации тайлов 0.0.1
 # +++++++
